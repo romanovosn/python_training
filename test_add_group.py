78db18ed3511a18ad98c99478b2638cea7d67b83
romanovosn <85156822+romanovosn@users.noreply.github.com>
2022-05-17 00:37:24 +0500

    группа параметров добавлена

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from group import Group
 import time
 
 class T1(unittest.TestCase):
@@ -14,7 +15,7 @@ class T1(unittest.TestCase):
         self.open_home_page(driver)
         self.login(driver, "admin", "secret")
         self.open_groups_page(driver)
-        self.create_group(driver, name="123", header="1234", footer="12345")
+        self.create_group(driver, Group(name="123", header="1234", footer="12345"))
         self.logout(driver)
 
     def test_t1(self):
@@ -22,25 +23,25 @@ class T1(unittest.TestCase):
         self.open_home_page(driver)
         self.login(driver, "admin", "secret")
         self.open_groups_page(driver)
-        self.create_group(driver, name="sdf", header="sdf", footer="sdffd")
+        self.create_group(driver, Group(name="sdf", header="sdf", footer="sdffd"))
         self.logout(driver)
         time.sleep(3)
 
     def logout(self, driver):
         driver.find_element_by_link_text("Logout").click()
 
-    def create_group(self, driver, name, header, footer):
+    def create_group(self, driver, group):
         # создание группы
         driver.find_element_by_name("new").click()
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys(name)
+        driver.find_element_by_name("group_name").send_keys(group.name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys(header)
+        driver.find_element_by_name("group_header").send_keys(group.header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys(footer)
+        driver.find_element_by_name("group_footer").send_keys(group.footer)
         # сохранение группы
         driver.find_element_by_name("submit").click()
         driver.find_element_by_link_text("group page").click()
