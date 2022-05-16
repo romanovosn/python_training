a7f9cd633e10fd455b8eb74f9bb78978e8339774
romanovosn <85156822+romanovosn@users.noreply.github.com>
2022-05-16 21:51:32 +0500

    вынес отдельно параметры теста

--- test_add_group.py
+++ test_add_group.py
@@ -9,30 +9,38 @@ class T1(unittest.TestCase):
         self.driver.implicitly_wait(30)
         self.verificationErrors = []
 
+    def test_t2(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, "admin", "secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, name="123", header="1234", footer="12345")
+        self.logout(driver)
+
     def test_t1(self):
         driver = self.driver
         self.open_home_page(driver)
-        self.login(driver)
+        self.login(driver, "admin", "secret")
         self.open_groups_page(driver)
-        self.create_group(driver)
+        self.create_group(driver, name="sdf", header="sdf", footer="sdffd")
         self.logout(driver)
-        time.sleep(5)
+        time.sleep(3)
 
     def logout(self, driver):
         driver.find_element_by_link_text("Logout").click()
 
-    def create_group(self, driver):
+    def create_group(self, driver, name, header, footer):
         # создание группы
         driver.find_element_by_name("new").click()
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("sdf")
+        driver.find_element_by_name("group_name").send_keys(name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("sdf")
+        driver.find_element_by_name("group_header").send_keys(header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("sdffd")
+        driver.find_element_by_name("group_footer").send_keys(footer)
         # сохранение группы
         driver.find_element_by_name("submit").click()
         driver.find_element_by_link_text("group page").click()
@@ -41,18 +49,18 @@ class T1(unittest.TestCase):
         # открытие группы
         driver.find_element_by_link_text("groups").click()
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         # логин
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, driver):
         # открытие страницы
         driver.get("http://localhost/addressbook/")
-        
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
